230da44bc312c8d35dfd73ddb0a579bef68e6b47
Nikitas Touloumtzis <p15toul@ionio.gr>
2018-12-14 14:26:26 +0200

    Update script_4.js

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -1,48 +1,105 @@
-var width = 960,
-    height = 500,
-    radius = Math.min(width, height) / 2;
+var svg = d3.select("svg"),
+         margin = 200, width = svg.attr("width") - margin,
+         height = svg.attr("height") - margin;
+         
+         svg.append("text")
+            .attr("transform", "translate(100,0)")
+            .attr("x", 50).attr("y", 50)
+            .attr("font-size", "20px")
+            .attr("class", "title")
+            .text("Population bar chart")
+            
+         var x = d3.scaleBand().range([0, width]).padding(0.4),
+         y = d3.scaleLinear().range([height, 0]);
+            
+         var g = svg.append("g")
+            .attr("transform", "translate(" + 100 + "," + 100 + ")");
 
-var color = d3.scale.category20();
+         d3.csv("data.csv", function(error, data) {
+            if (error) {
+               throw error;
+            }
+               
+            x.domain(data.map(function(d) { return d.year; }));
+            y.domain([0, d3.max(data, function(d) { return d.population; })]);
+                     
+            g.append("g")
+               .attr("transform", "translate(0," + height + ")")
+               .call(d3.axisBottom(x))
+               .append("text")
+               .attr("y", height - 250)
+               .attr("x", width - 100)
+               .attr("text-anchor", "end")
+               .attr("font-size", "18px")
+               .attr("stroke", "blue").text("year");
+               
+            g.append("g")
+               .append("text")
+               .attr("transform", "rotate(-90)")
+               .attr("y", 6)
+               .attr("dy", "-5.1em")
+               .attr("text-anchor", "end")
+               .attr("font-size", "18px")
+               .attr("stroke", "blue")
+               .text("population");
+                         
+            g.append("g")
+               .attr("transform", "translate(0, 0)")
+               .call(d3.axisLeft(y))
 
-var pie = d3.layout.pie()
-    .value(function(d) { return d.apples; })
-    .sort(null);
-
-var arc = d3.svg.arc()
-    .innerRadius(radius - 100)
-    .outerRadius(radius - 20);
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-d3.tsv("assets/data/species.tsv", type, function(error, data) {
-  var path = svg.datum(data).selectAll("path")
-      .data(pie)
-    .enter().append("path")
-      .attr("fill", function(d, i) { return color(i); })
-      .attr("d", arc);
-
-  d3.selectAll("input")
-      .on("change", change);
-
-  var timeout = setTimeout(function() {
-    d3.select("input[value=\"oranges\"]").property("checked", true).each(change);
-  }, 2000);
-
-  function change() {
-    var value = this.value;
-    clearTimeout(timeout);
-    pie.value(function(d) { return d[value]; }); // change the value function
-    path = path.data(pie); // compute the new angles
-    path.attr("d", arc); // redraw the arcs
-  }
-});
-
-function type(d) {
-  d.apples = +d.apples;
-  d.oranges = +d.oranges;
-  return d;
-}
+            g.selectAll(".bar")
+               .data(data)
+               .enter()
+               .append("rect")
+               .attr("class", "bar")
+               .on("mouseover", onMouseOver) 
+               .on("mouseout", onMouseOut)   
+               .attr("x", function(d) { return x(d.year); })
+               .attr("y", function(d) { return y(d.population); })
+               .attr("width", x.bandwidth()).transition()
+               .ease(d3.easeLinear).duration(200)
+               .delay(function (d, i) {
+                  return i * 25;
+               })
+                  
+            .attr("height", function(d) { return height - y(d.population); });
+         });
+          
+          
+         function onMouseOver(d, i) {
+            d3.select(this)
+            .attr('class', 'highlight');
+               
+            d3.select(this)
+               .transition()     
+               .duration(200)
+               .attr('width', x.bandwidth() + 5)
+               .attr("y", function(d) { return y(d.population) - 10; })
+               .attr("height", function(d) { return height - y(d.population) + 10; });
+              
+            g.append("text")
+               .attr('class', 'val')
+               .attr('x', function() {
+                  return x(d.year);
+               })
+               
+            .attr('y', function() {
+               return y(d.value) - 10;
+            })
+         }
+          
+         function onMouseOut(d, i) {
+             
+            d3.select(this)
+               .attr('class', 'bar');
+            
+            d3.select(this)
+               .transition()     
+               .duration(200)
+               .attr('width', x.bandwidth())
+               .attr("y", function(d) { return y(d.population); })
+               .attr("height", function(d) { return height - y(d.population); });
+            
+            d3.selectAll('.val')
+               .remove()
+         }
