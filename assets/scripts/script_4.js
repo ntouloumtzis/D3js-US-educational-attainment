a23bce7fee12409483c933936f7ce25efce26eca
Nikitas Touloumtzis <p15toul@ionio.gr>
2018-12-15 06:55:56 +0200

    Update script_4.js

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -1,105 +1,60 @@
-var svg = d3.select("svg"),
-         margin = 200, width = svg.attr("width") - margin,
-         height = svg.attr("height") - margin;
-         
-         svg.append("text")
-            .attr("transform", "translate(100,0)")
-            .attr("x", 50).attr("y", 50)
-            .attr("font-size", "20px")
-            .attr("class", "title")
-            .text("Population bar chart")
-            
-         var x = d3.scaleBand().range([0, width]).padding(0.4),
-         y = d3.scaleLinear().range([height, 0]);
-            
-         var g = svg.append("g")
-            .attr("transform", "translate(" + 100 + "," + 100 + ")");
-
-         d3.csv("assets/data/monimos_pluthismos_kata_fulo_perifereia.csv", function(error, data) {
-            if (error) {
-               throw error;
-            }
-               
-            x.domain(data.map(function(d) { return d.year; }));
-            y.domain([0, d3.max(data, function(d) { return d.population; })]);
-                     
-            g.append("g")
-               .attr("transform", "translate(0," + height + ")")
-               .call(d3.axisBottom(x))
-               .append("text")
-               .attr("y", height - 250)
-               .attr("x", width - 100)
-               .attr("text-anchor", "end")
-               .attr("font-size", "18px")
-               .attr("stroke", "blue").text("year");
-               
-            g.append("g")
-               .append("text")
-               .attr("transform", "rotate(-90)")
-               .attr("y", 6)
-               .attr("dy", "-5.1em")
-               .attr("text-anchor", "end")
-               .attr("font-size", "18px")
-               .attr("stroke", "blue")
-               .text("population");
-                         
-            g.append("g")
-               .attr("transform", "translate(0, 0)")
-               .call(d3.axisLeft(y))
-
-            g.selectAll(".bar")
-               .data(data)
-               .enter()
-               .append("rect")
-               .attr("class", "bar")
-               .on("mouseover", onMouseOver) 
-               .on("mouseout", onMouseOut)   
-               .attr("x", function(d) { return x(d.year); })
-               .attr("y", function(d) { return y(d.population); })
-               .attr("width", x.bandwidth()).transition()
-               .ease(d3.easeLinear).duration(200)
-               .delay(function (d, i) {
-                  return i * 25;
-               })
-                  
-            .attr("height", function(d) { return height - y(d.population); });
-         });
-          
-          
-         function onMouseOver(d, i) {
-            d3.select(this)
-            .attr('class', 'highlight');
-               
-            d3.select(this)
-               .transition()     
-               .duration(200)
-               .attr('width', x.bandwidth() + 5)
-               .attr("y", function(d) { return y(d.population) - 10; })
-               .attr("height", function(d) { return height - y(d.population) + 10; });
-              
-            g.append("text")
-               .attr('class', 'val')
-               .attr('x', function() {
-                  return x(d.year);
-               })
-               
-            .attr('y', function() {
-               return y(d.value) - 10;
-            })
-         }
-          
-         function onMouseOut(d, i) {
-             
-            d3.select(this)
-               .attr('class', 'bar');
-            
-            d3.select(this)
-               .transition()     
-               .duration(200)
-               .attr('width', x.bandwidth())
-               .attr("y", function(d) { return y(d.population); })
-               .attr("height", function(d) { return height - y(d.population); });
-            
-            d3.selectAll('.val')
-               .remove()
-         }
+// Set the dimensions of the canvas / graph
+var	margin = {top: 30, right: 20, bottom: 30, left: 50},
+	width = 600 - margin.left - margin.right,
+	height = 270 - margin.top - margin.bottom;
+ 
+// Parse the date / time
+var	parseDate = d3.time.format("%d-%b-%y").parse;
+ 
+// Set the ranges
+var	x = d3.time.scale().range([0, width]);
+var	y = d3.scale.linear().range([height, 0]);
+ 
+// Define the axes
+var	xAxis = d3.svg.axis().scale(x)
+	.orient("bottom").ticks(5);
+ 
+var	yAxis = d3.svg.axis().scale(y)
+	.orient("left").ticks(5);
+ 
+// Define the line
+var	valueline = d3.svg.line()
+	.x(function(d) { return x(d.date); })
+	.y(function(d) { return y(d.close); });
+    
+// Adds the svg canvas
+var	svg = d3.select("body")
+	.append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+ 
+// Get the data
+d3.csv("assets/data/pososta_anergias.csv", function(error, data) {
+	data.forEach(function(d) {
+		d.date = parseDate(d.date);
+		d.close = +d.close;
+	});
+ 
+	// Scale the range of the data
+	x.domain(d3.extent(data, function(d) { return d.date; }));
+	y.domain([0, d3.max(data, function(d) { return d.close; })]);
+ 
+	// Add the valueline path.
+	svg.append("path")	
+		.attr("class", "line")
+		.attr("d", valueline(data));
+ 
+	// Add the X Axis
+	svg.append("g")		
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+ 
+	// Add the Y Axis
+	svg.append("g")		
+		.attr("class", "y axis")
+		.call(yAxis);
+ 
+});
