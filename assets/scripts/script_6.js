4baed244acc5c34126d188d9af012a60c0aff7f8
Nikitas Touloumtzis <p15toul@ionio.gr>
2018-12-16 16:31:34 +0200

    Update script_6.js

--- assets/scripts/script_6.js
+++ assets/scripts/script_6.js
@@ -1,54 +1,69 @@
-var margin = {top: 20, right: 30, bottom: 40, left: 30},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var data = [
+  {month: "Είδη Διατροφής", apples: 5.6},
+  {month: "", apples: 0},
+  {month: "Kαύσιμα/λιπαντικά αυτοκινήτων", apples: 1.0},
+  {month: " ", apples: 0},
+  {month: "Λοιπά καταστήματα", apples:  1.0},
+  {month: "  ", apples: 0},
+  {month: "Μεγάλα καταστήματα τροφίμων", apples:  5.9},
+  {month: "   ", apples: 0},
+  {month: "Πολυκαταστήματα", dates: -5.2},
+  {month: "Τρόφιμα/Ποτά/Καπνός", apples: 3.9},
+  {month: "Φαρμακευτικά/Καλλυντικά", dates:  -1.5},
+  {month: "Ένδυση – Υπόδηση", apples:  4.7},
+  {month: "Έπιπλα/Ηλεκτρικά είδη/Οικιακός εξοπλισμός", apples:  1.1},
+  {month: "Βιβλία/Χαρτικά/Λοιπά είδη", apples:  7.4},
+  {month: "Γενικός Δείκτης", apples:  3.3},
+  {month: "Γενικός Δείκτης, εκτός καυσίμων και λιπαντικών αυτοκινήτων", apples:  3.4}
+];
 
-var x = d3.scale.linear()
-    .range([0, width]);
+var series = d3.stack()
+    .keys(["apples","dates"])
+    .offset(d3.stackOffsetDiverging)
+    (data);
 
-var y = d3.scale.ordinal()
-    .rangeRoundBands([0, height], 0.1);
+var svg = d3.select("svg"),
+    margin = {top: 50, right: 30, bottom: 50, left: 30},
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+var x = d3.scaleBand()
+    .domain(data.map(function(d) { return d.month; }))
+    .rangeRound([margin.left, width - margin.right])
+    .padding(0.4);
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .tickSize(0)
-    .tickPadding(6);
+var y = d3.scaleLinear()
+    .domain([d3.min(series, stackMin), d3.max(series, stackMax)])
+    .rangeRound([height - margin.bottom, margin.top]);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var z = d3.scaleOrdinal(d3.schemeCategory10);
 
-d3.tsv("assets/data/lianiko_emporio.tsv", type, function(error, data) {
-  x.domain(d3.extent(data, function(d) { return d.value; })).nice();
-  y.domain(data.map(function(d) { return d.name; }));
+svg.append("g")
+  .selectAll("g")
+  .data(series)
+  .enter().append("g")
+    .attr("fill", function(d) { return z(d.key); })
+  .selectAll("rect")
+  .data(function(d) { return d; })
+  .enter().append("rect")
+    .attr("width", x.bandwidth)
+    .attr("x", function(d) { return x(d.data.month); })
+    .attr("y", function(d) { return y(d[1]); })
+    .attr("height", function(d) { return y(d[0]) - y(d[1]); })
 
-  svg.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", function(d) { return "bar bar--" + (d.value < 0 ? "negative" : "positive"); })
-      .attr("x", function(d) { return x(Math.min(0, d.value)); })
-      .attr("y", function(d) { return y(d.name); })
-      .attr("width", function(d) { return Math.abs(x(d.value) - x(0)); })
-      .attr("height", y.rangeBand());
+svg.append("g")
+    .attr("transform", "translate(0," + y(0) + ")")
+    .call(d3.axisBottom(x));
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+svg.append("g")
+    .attr("transform", "translate(" + margin.left + ",0)")
+    .call(d3.axisLeft(y));
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + x(0) + ",0)")
-      .call(yAxis);
-});
-
-function type(d) {
-  d.value = +d.value;
-  return d;
+function stackMin(serie) {
+  return d3.min(serie, function(d) { return d[0]; });
 }
+
+function stackMax(serie) {
+  return d3.max(serie, function(d) { return d[1]; });
+}
+
