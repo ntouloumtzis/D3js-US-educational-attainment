1175fdb22bb99310614699195a7297ecdef6be54
Nikitas Touloumtzis <p15toul@ionio.gr>
2018-12-16 13:15:29 +0200

    Update script_5.js

--- assets/scripts/script_5.js
+++ assets/scripts/script_5.js
@@ -1,46 +1,9 @@
-(function (d3) {
-  'use strict';
-
-  const svg = d3.select('svg');
-
-  const width = +svg.attr('width');
-  const height = +svg.attr('height');
-
-  const render = data => {
-    const xValue = d => d.death;
-    const yValue = d => d.country;
-    const margin = { top: 20, right: 40, bottom: 20, left: 100 };
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-    
-    const xScale = d3.scaleLinear()
-      .domain([0, d3.max(data, xValue)])
-      .range([0, innerWidth]);
-    
-    const yScale = d3.scaleBand()
-      .domain(data.map(yValue))
-      .range([0, innerHeight])
-      .padding(0.1);
-    
-    const g = svg.append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
-    
-    g.append('g').call(d3.axisLeft(yScale));
-    g.append('g').call(d3.axisBottom(xScale))
-      .attr('transform', `translate(0,${innerHeight})`);
-    
-    g.selectAll('rect').data(data)
-      .enter().append('rect')
-        .attr('y', d => yScale(yValue(d)))
-        .attr('width', d => xScale(xValue(d)))
-        .attr('height', yScale.bandwidth());
-  };
-
-  d3.csv('assets/data/script1_3.csv').then(data => {
-    data.forEach(d => {
-      d.death = (d.death/5210205)*100;
-    });
-    render(data);
-  });
-
-}(d3));
+var chart = c3.generate({
+  bindto: d3.select('.chart'),
+  data: {
+    columns: [
+      ['2016', 93.8 , 95.1 , 95.7, 97.1],
+      ['2017', 96.6 , 98.4 , 97.8, 97.8]
+    ]
+  }
+});
