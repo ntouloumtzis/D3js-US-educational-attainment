988d15feaf2b3ed5dcd8af419c2fb447ae6a1a34
Nikitas Touloumtzis <p15toul@ionio.gr>
2018-12-16 11:47:49 +0200

    Update script_4.js

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -1,18 +1,3 @@
-//Regular pie chart example
-nv.addGraph(function() {
-  var chart = nv.models.pieChart()
-      .x(function(d) { return d.label })
-      .y(function(d) { return d.value })
-      .showLabels(true);
-
-    d3.select("#chart svg")
-        .datum(exampleData())
-        .transition().duration(350)
-        .call(chart);
-
-  return chart;
-});
-
 //Donut chart example
 nv.addGraph(function() {
   var chart = nv.models.pieChart()
@@ -70,3 +55,4 @@ function exampleData() {
       }
     ];
 }
+
