c53992c6a7b4574e2ef772046e0fd3ad24ee0941
Nikitas Touloumtzis <p15toul@ionio.gr>
2018-12-17 14:06:21 +0200

    Update script_4.js

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -13,9 +13,9 @@ nv.addGraph(function() {
     d3.select("#chart2 svg")
         .datum(exampleData())
         .transition().duration(350)
-        .call(chart);
+        .call(chart2);
 
-  return chart;
+  return chart2;
 });
 
 //Pie chart example data. Note how there is only a single array of key-value pairs.
